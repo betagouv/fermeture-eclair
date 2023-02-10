286192eb2511ac4c2241b7bee303337c407dec1e
BenoitSerrano <benoit.serrano10@gmail.com>
2023-02-10 16:28:03 +0100

    add test before parsing

--- src/lib/buildController.ts
+++ src/lib/buildController.ts
@@ -22,7 +22,7 @@ function buildController<bodyT>(
                 return;
             }
         }
-        const body = JSON.parse(req.body);
+        const body = req.body ? JSON.parse(req.body) : {};
 
         if (options?.schema) {
             const { error } = options.schema.validate(body);
