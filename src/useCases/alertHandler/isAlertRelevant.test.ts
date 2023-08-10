a1858d2795b687af0f232bd65ef12142ea29ffb8
Benoit Serrano <benoit.serrano10@gmail.com>
2023-08-10 21:06:31 +0200

    fix typing error

--- src/useCases/alertHandler/isAlertRelevant.test.ts
+++ src/useCases/alertHandler/isAlertRelevant.test.ts
@@ -5,6 +5,7 @@ describe('isAlertRelevant', () => {
     it('returns false for generic password', () => {
         const payload = {
             occurrence: {
+                author_info: 'test@test.fr',
                 source: { url: 'https://github.com/tchapgouv/tchap-android/commit/d4ebefbab' },
             },
             incident: { detector: { name: 'generic_password' } },
