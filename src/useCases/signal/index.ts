25bd526f10276b34e3e215e5be46dde2a8e79846
Benoit Serrano <benoit.serrano10@gmail.com>
2023-08-12 21:26:59 -0700

    fix florian name tag

--- src/useCases/signal/index.ts
+++ src/useCases/signal/index.ts
@@ -8,7 +8,7 @@ const MATTERMOST_BASE_URL = `https://mattermost.incubateur.net/hooks/`;
 
 function alertOnMattermost(payload: alertPayloadType) {
     const url = MATTERMOST_BASE_URL + config.MATTERMOST_HOOK_ID;
-    const text = `Bonjour @anna-livia @julien.dauphant @ishan @florian.delezenne @benoit.serrano.\nUne alerte a été levée sur le repository ${payload.occurrence.source.url}. L'auteur du commit est ${payload.occurrence.author_info} : contactez cette personne pour plus d'informations sur la fuite.`;
+    const text = `Bonjour @anna-livia @julien.dauphant @ishan @florian @benoit.serrano.\nUne alerte a été levée sur le repository ${payload.occurrence.source.url}. L'auteur du commit est ${payload.occurrence.author_info} : contactez cette personne pour plus d'informations sur la fuite.`;
     return axios.post(url, { text });
 }
 
