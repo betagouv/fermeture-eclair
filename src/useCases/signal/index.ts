4c53a203cb2bdf9856e13652494425c6643a1754
Benoit Serrano <benoit.serrano10@gmail.com>
2023-08-31 11:10:10 +0100

    improve design message for mattermost

--- src/useCases/signal/index.ts
+++ src/useCases/signal/index.ts
@@ -10,16 +10,23 @@ const usernamesToPing = ['anna-liva', 'julien.dauphant', 'ishan', 'florian', 'be
 
 async function alertOnMattermost(payload: alertPayloadType) {
     const url = MATTERMOST_BASE_URL + config.MATTERMOST_HOOK_ID;
-    const text1 = `Bonjour @anna-livia @julien.dauphant @ishan @florian @benoit.serrano.\nUne alerte a été déclenchée (${payload.incident.gitguardian_url}) sur le repository ${payload.occurrence.source.url}. L'auteur du commit est ${payload.occurrence.author_info} : contactez cette personne pour plus d'informations sur la fuite.`;
-    await axios.post(url, { text: text1 });
 
-    const text2 = `
-    # 🚨 GitGuardian alert on "${payload.occurrence.source.full_name}"\n
-    - 🦉 GitGuardian URL: ${payload.incident.gitguardian_url}\n
-    - 🐱 Github commit URL: ${payload.occurrence.source.url}\n
-    - 👩‍💻 developer's email adress: ${payload.occurrence.author_info}\n
-    ping ${usernamesToPing.map((username) => `@${username}`).join(' ')}.\n`;
-    return axios.post(url, { text: text2 });
+    const text =
+        '# 🚨 GitGuardian alert on ' +
+        payload.occurrence.source.full_name +
+        '\n' +
+        '- 🦉 GitGuardian URL: ' +
+        payload.incident.gitguardian_url +
+        '\n' +
+        '- 🐱 Github commit URL: ' +
+        payload.occurrence.source.url +
+        '\n' +
+        "- 👩‍💻 developer's email adress: " +
+        payload.occurrence.author_info +
+        '\n' +
+        'ping ' +
+        usernamesToPing.map((username) => `@${username}`).join(' ');
+    return axios.post(url, { text: text });
 }
 
 export { signalUseCase };
